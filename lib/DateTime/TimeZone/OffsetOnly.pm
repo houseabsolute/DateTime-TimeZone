0bab4726cd0f293d1e7fbb51980bb1c3ee45783a
autarch <devnull@localhost>
2003-02-11 00:53:18 +0000

    add is_dst_for_datetime method

--- lib/DateTime/TimeZone/OffsetOnly.pm
+++ lib/DateTime/TimeZone/OffsetOnly.pm
@@ -28,6 +28,8 @@ sub new
 
 sub is_utc { $_[0]->{offset} ? 0 : 1 }
 
+sub is_dst_for_datetime { 0 }
+
 sub offset_for_datetime { $_[0]->{offset} }
 sub offset_for_local_datetime { $_[0]->{offset} }
 
