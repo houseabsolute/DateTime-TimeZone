6e1ff89b97e8f90e38afcda0d16e49dd73287de0
autarch <devnull@localhost>
2003-02-15 05:33:19 +0000

    latest version

--- lib/DateTime/TimeZone.pm
+++ lib/DateTime/TimeZone.pm
@@ -3,7 +3,7 @@ package DateTime::TimeZone;
 use strict;
 
 use vars qw( $VERSION $INFINITY $NEG_INFINITY );
-$VERSION = '0.05';
+$VERSION = '0.06';
 
 use DateTime::TimeZoneCatalog;
 use DateTime::TimeZone::Floating;
