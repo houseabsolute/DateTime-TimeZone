a27d63c9c7829084e1f9e9b6c2edbfb77de6fffb
autarch <devnull@localhost>
2007-05-07 15:54:31 +0000

    Update docs to reflect change in strategy.

--- lib/DateTime/TimeZone/Local/Unix.pm
+++ lib/DateTime/TimeZone/Local/Unix.pm
@@ -271,9 +271,10 @@ If this file is a symlink to an Olson database time zone file (usually
 in F</usr/share/zoneinfo>) then it uses the target file's path to
 determine the time zone name.
 
-If this file is a copy of a zoneinfo file, it looks for a file that
-matches this file in F</usr/share/zoneinfo>. If it finds one, it uses
-that file's path to determine the time zone name.
+If reading the symlink fails for some reason, or if this file is not a
+symlink, it looks for a file that matches this file in
+F</usr/share/zoneinfo>. If it finds one, it uses that file's path to
+determine the time zone name.
 
 =item * F</etc/timezone>
 
