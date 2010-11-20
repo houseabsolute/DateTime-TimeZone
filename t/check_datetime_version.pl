fced5c72fc4d7e70df038ccfa20a1fbfa9ac417b
Dave Rolsky <autarch@urth.org>
2010-11-20 00:03:07 -0600

    Work around not having a $VERSION when running tests with dzil

--- t/check_datetime_version.pl
+++ t/check_datetime_version.pl
@@ -3,6 +3,11 @@ use strict;
 use DateTime::TimeZone;
 
 BEGIN {
+    # Work around not having a version when using dzil
+    if ( ! $DateTime::TimeZone::VERSION ) {
+        $DateTime::TimeZone::VERSION = 99;
+    }
+
     my $version = '0.1501';
     eval "use DateTime $version";
     if ($@) {
