3e5a1f9abbc40da8db8650814935e7f1639ce9b1
Dave Rolsky <autarch@urth.org>
2016-11-02 23:11:30 -0500

    Bump version after release

--- lib/DateTime/TimeZone.pm
+++ lib/DateTime/TimeZone.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.07';
+our $VERSION = '2.08';
 
 use DateTime::TimeZone::Catalog;
 use DateTime::TimeZone::Floating;
--- lib/DateTime/TimeZone/OlsonDB/Change.pm
+++ lib/DateTime/TimeZone/OlsonDB/Change.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.07';
+our $VERSION = '2.08';
 
 sub new {
     my $class = shift;
