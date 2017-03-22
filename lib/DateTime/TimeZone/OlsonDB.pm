d784acf61536d82f35ce35d59c4c85fd435adf09
Dave Rolsky <autarch@urth.org>
2017-03-22 14:21:10 +0800

    Bump version after release

--- lib/DateTime/TimeZone/OlsonDB.pm
+++ lib/DateTime/TimeZone/OlsonDB.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.11';
+our $VERSION = '2.12';
 
 use DateTime::TimeZone::OlsonDB::Rule;
 use DateTime::TimeZone::OlsonDB::Zone;
