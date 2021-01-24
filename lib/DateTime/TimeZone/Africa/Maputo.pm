ea5c57d62be0e47d3c3e3783e3e013259b7c0b95
Dave Rolsky <autarch@urth.org>
2021-01-24 14:55:49 -0600

    Bump version after release

--- lib/DateTime/TimeZone/Africa/Maputo.pm
+++ lib/DateTime/TimeZone/Africa/Maputo.pm
@@ -13,7 +13,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.47';
+our $VERSION = '2.48';
 
 use Class::Singleton 1.03;
 use DateTime::TimeZone;
--- lib/DateTime/TimeZone/Catalog.pm
+++ lib/DateTime/TimeZone/Catalog.pm
@@ -11,7 +11,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.47';
+our $VERSION = '2.48';
 
 our @ALL =
 qw(
