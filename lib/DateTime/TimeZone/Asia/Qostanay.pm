e61e5a22613903b339b323c81b7d4dc3b41b2215
Dave Rolsky <autarch@urth.org>
2022-09-24 10:32:40 -0500

    Bump version after release

--- lib/DateTime/TimeZone/Asia/Qostanay.pm
+++ lib/DateTime/TimeZone/Asia/Qostanay.pm
@@ -13,7 +13,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.54';
+our $VERSION = '2.55';
 
 use Class::Singleton 1.03;
 use DateTime::TimeZone;
--- lib/DateTime/TimeZone/Catalog.pm
+++ lib/DateTime/TimeZone/Catalog.pm
@@ -11,7 +11,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.54';
+our $VERSION = '2.55';
 
 our @ALL =
 qw(
