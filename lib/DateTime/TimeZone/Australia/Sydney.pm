3349ed92917f1b18d30372b4311b59c8e950ebb6
Dave Rolsky <autarch@urth.org>
2023-03-23 10:07:47 -0500

    Bump version after release

--- lib/DateTime/TimeZone/Australia/Sydney.pm
+++ lib/DateTime/TimeZone/Australia/Sydney.pm
@@ -13,7 +13,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.58';
+our $VERSION = '2.59';
 
 use Class::Singleton 1.03;
 use DateTime::TimeZone;
--- lib/DateTime/TimeZone/Catalog.pm
+++ lib/DateTime/TimeZone/Catalog.pm
@@ -11,7 +11,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.58';
+our $VERSION = '2.59';
 
 our @ALL =
 qw(
