ac470bb198916020752b96304b231f2eb739e310
Dave Rolsky <autarch@urth.org>
2024-02-02 10:15:11 +0800

    Regen code for 2024a release and update Changes

--- lib/DateTime/TimeZone/Catalog.pm
+++ lib/DateTime/TimeZone/Catalog.pm
@@ -1863,7 +1863,7 @@ our %LINKS =
 
 ;
 
-sub OlsonVersion { '2023d' }
+sub OlsonVersion { '2024a' }
 
 
 1;
@@ -2950,7 +2950,7 @@ so that applications can easily present a list of timezones.
 
   Asia/Kathmandu
 
-=head3 Netherlands (NL)
+=head3 Netherlands (Kingdom of the) (NL)
 
   Europe/Amsterdam
 
