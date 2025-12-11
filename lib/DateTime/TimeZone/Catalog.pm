651260169138ea44e61675aff923195f5a8139b9
Dave Rolsky <autarch@urth.org>
2025-12-11 09:50:51 +0800

    Bump version after release

--- lib/DateTime/TimeZone/Catalog.pm
+++ lib/DateTime/TimeZone/Catalog.pm
@@ -11,7 +11,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.66';
+our $VERSION = '2.67';
 
 our @ALL =
 qw(
--- lib/DateTime/TimeZone/Indian/Mauritius.pm
+++ lib/DateTime/TimeZone/Indian/Mauritius.pm
@@ -13,7 +13,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '2.66';
+our $VERSION = '2.67';
 
 use Class::Singleton 1.03;
 use DateTime::TimeZone;
